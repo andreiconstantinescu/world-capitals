ab84080da067ba6278cac7c1713a3e8609b54660
Andrei <andrei@constantinescu.io>
2016-10-15 21:36:27 +0100

    Format.

--- src/handlers/newSession.js
+++ src/handlers/newSession.js
@@ -4,14 +4,14 @@ const _ = require('lodash')
 const constants = require('../constants/constants.js')
 
 const newSessionHandlers = {
-  NewSession () {
+  NewSession() {
     if (!_.length(_.keys(this.atrributes))) {
       this.attributes.gamesPlayed = 0
     }
     this.handler.state = constants.skillStates.STARTMODE
     this.emit(':ask',
-              `Welcome to  ${constants.appInfo.SKILL_NAME}! Do you want to play?`,
-              'Say yes to begin the fun or no to quit.')
+      `Welcome to  ${constants.appInfo.SKILL_NAME}! Do you want to play?`,
+      'Say yes to begin the fun or no to quit.')
   }
 }
 
