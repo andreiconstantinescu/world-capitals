cae66623e48b0b054aff674a67f14d44b2dfd36b
Andrei <andrei@constantinescu.io>
2016-10-22 19:22:14 +0100

    Centralize resposes.

---
+++ assets/responses.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const states = require('../src/constants/states.js')
+
+const positive = [
+  'Congratulations!',
+  'Perfect!',
+  'Wonderful!',
+  'Great!'
+]
+
+const negative = [
+  'Too bad',
+  'Unfortunately',
+  'Unlucky'
+]
+
+const helpful = {
+  [states.STARTMODE]: 'Say new for a new game, no to end the game or help to find out more information.',
+  [states.PLAYMODE]: 'Say repeat to repeat the question, new to start a new game or end to end the game.'
+}
+
+const unhandled = {
+  [states.STARTMODE]: 'Say new for a new game, no to end the game or help to find out more information.'
+}
+
+const exit = [
+  'Alright, see you next time!',
+  'See you next time!',
+  'I hope you had fun. Bye!'
+]
+
+module.exports = {
+  positive,
+  negative,
+  helpful,
+  unhandled,
+  exit
+}
